64cedc57a4f6679591024e3a0dba2f63ed021ee4
ahmadfatan <ahmad.fatan.tif423@polban.ac.id>
2024-04-21 18:09:09 +0700

    adding nav bar in scraping.html

--- js/scraping.js
+++ js/scraping.js
@@ -57,3 +57,22 @@ $(document).ready(function() {
         $(this).siblings('.info').slideToggle();
     });
 });
+
+/*=============== NAV BAR ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
